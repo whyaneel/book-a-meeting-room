4aaed9279e85dfb17243e720c65a902431898275
bhanukiran <chitikelabhanukiran@gmail.com>
2019-11-11 11:39:51 +0800

    [Bhanu] Updated the Gateway service and refactored entities as per the PR review comments.

--- src/main/java/com/prototype/booking/BookAMeetingRoomApplication.java
+++ src/main/java/com/prototype/booking/BookAMeetingRoomApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
-@EnableIntegration
 public class BookAMeetingRoomApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
